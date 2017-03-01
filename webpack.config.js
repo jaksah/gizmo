9351b093ca7364258a51ac15ed851006e9af37e2
Jakob Sahlström <jakob.sahlstrom@gmail.com>
2017-03-01 22:21:10 +0100

    Restart with simple typescript + webpack + redux setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,29 @@
-var path = require('path');
 var webpack = require('webpack');
+var path = require('path');
 
-var config = {
-  devtool: 'cheap-module-eval-source-map',
-  entry: [
-    'webpack-hot-middleware/client',
-    './app/main'
-  ],
-  output: {
-    path: path.join(__dirname, 'public', 'js'),
-    filename: 'bundle.js',
-    publicPath: '/js'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    })
-  ],
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          plugins: [
-            ['react-transform', {
-              transforms: [
-                {
-                  transform: 'react-transform-hmr',
-                  imports: ['react'],
-                  locals: ['module']
-                }, {
-                  transform: 'react-transform-catch-errors',
-                  imports: ['react', 'redbox-react']
-                }
-              ]
-            }]
-          ]
-        }
-      }
-    ]
-  }
+var SRC_DIR = path.join(__dirname, 'src');
+
+module.exports = {
+    entry: ['./src/app.tsx'],
+    module: {
+        loaders: [{
+            test: /\.tsx?$/,
+            loader: 'awesome-typescript-loader',
+            include: SRC_DIR
+        }]
+    },
+    output: {
+        filename: 'app.js',
+        path: path.join(__dirname, 'build')
+    },
+    devtool: 'source-map',
+    plugins: [
+    ],
+    resolve: {
+        modules: [
+            SRC_DIR,
+            "node_modules"
+        ],
+        extensions: ['.jsx', '.js', '.tsx', '.ts']
+    }
 };
-
-if (process.env.NODE_ENV === 'production') {
-  config.plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        screw_ie8: true,
-        warnings: false
-      }
-    })
-  );
-}
-
-module.exports = config;
